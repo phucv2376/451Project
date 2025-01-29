93afa3533ef7fca8c70b64f827556369495c0294
AbdalrahmanBashir <abdalr.bashir@gmail.com>
2025-01-29 14:38:51 -0600

    feat(domain): add base classes for DDD entities and value objects
    - Implemented `ValueObject` class with component-based equality logic.
    - Added `Entity<TId>` class with equality operations and ORM-friendly support.
    - Created `AggregateRoot<TId>` class extending `Entity<TId>`, representing aggregate roots.
    - Included equality overrides (`Equals`, `GetHashCode`) and parameterless constructors for ORM compatibility.
    
    These base classes provide the foundation for implementing DDD concepts in the domain model.

---
+++ BudgetAppBackend.Domain/Commons/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿namespace BudgetAppBackend.Domain.Commons
+{
+    public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+    {
+
+
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+
+        private AggregateRoot() : base(default)
+        {
+
+            // It's mainly used for ORM frameworks that require a parameterless constructor.
+        }
+
+
+    }
+}
