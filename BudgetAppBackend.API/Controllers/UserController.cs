e6b240a7b4bcc5a2f98d94a97add2cd356ca852a
AbdalrahmanBashir <abdalr.bashir@gmail.com>
2025-01-29 14:42:05 -0600

    feat(Controllers): adding a user controller

---
+++ BudgetAppBackend.API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetAppBackend.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
