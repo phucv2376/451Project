66864c53529f9334d0560f05d336b012c2978402
AbdalrahmanBashir <abdalr.bashir@gmail.com>
2025-02-01 00:23:45 -0600

    Update ResetPasswordDto to include any new fields or requirements for improved password handling.

--- BudgetAppBackend.Application/DTOs/AuthenticationDTOs/ResetPasswordDto.cs
+++ BudgetAppBackend.Application/DTOs/AuthenticationDTOs/ResetPasswordDto.cs
@@ -1,9 +1,14 @@
-﻿namespace BudgetAppBackend.Application.DTOs.AuthenticationDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetAppBackend.Application.DTOs.AuthenticationDTOs
 {
     public class ResetPasswordDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string NewPassword { get; set; }
+        [Required]
         public string ConfirmNewPassword { get; set; }
     }
 }
