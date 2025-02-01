72d7251377503598ce7bfb1c3336fb73e676a6c9
AbdalrahmanBashir <abdalr.bashir@gmail.com>
2025-02-01 00:24:06 -0600

    Enhance VerifyEmailDto to support email verification scenarios with domain events.

--- BudgetAppBackend.Application/DTOs/AuthenticationDTOs/VerifyEmailDto.cs
+++ BudgetAppBackend.Application/DTOs/AuthenticationDTOs/VerifyEmailDto.cs
@@ -1,8 +1,12 @@
-﻿namespace BudgetAppBackend.Application.DTOs.AuthenticationDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetAppBackend.Application.DTOs.AuthenticationDTOs
 {
     public class VerifyEmailDto
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Code { get; set; }
     }
 }
