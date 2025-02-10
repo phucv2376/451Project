f6e8aeb2d7fbbad6aec1835cc1ce188598083001
AbdalrahmanBashir <abdalr.bashir@gmail.com>
2025-02-10 15:47:33 -0600

    Adding a clean UserAggregateTests Project

---
+++ BudgetAppBackend.Domain.Tests/UserAggregateTests.cs
@@ -0,0 +1,93 @@
+﻿using BudgetAppBackend.Domain.UserAggregate;
+
+namespace BudgetAppBackend.Domain.Tests
+{
+    public class UserAggregateTests
+    {
+        [Fact]
+        public void CreateNewUser_ShouldCreateUser_WithValidInputs()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+
+        [Fact]
+        public void ChangePassword_ShouldUpdatePasswordHash()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void VerifyPassword_ShouldReturnTrue_ForCorrectPassword()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void VerifyPassword_ShouldReturnFalse_ForIncorrectPassword()
+        {
+            // Arrange
+            
+            // Act
+          
+            // Assert
+           
+        }
+
+        [Fact]
+        public void SetEmailVerificationCode_ShouldRaiseEvent()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void VerifyEmail_ShouldReturnTrue_ForValidCodeAndNotExpired()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void VerifyEmail_ShouldReturnFalse_ForInvalidCode()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+        [Fact]
+        public void VerifyEmail_ShouldReturnFalse_ForExpiredCode()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
