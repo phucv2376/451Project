39f26279e3f96861191d2423e8c2fadabdefec34
Abdalrahman H Bashir <127059452+AbdalrahmanBashir@users.noreply.github.com>
2025-03-06 02:51:08 -0600

    Update AutoMapper config and application project file

--- BudgetAppBackend.Application/AutoMapper/ConfigAutoMapper.cs
+++ BudgetAppBackend.Application/AutoMapper/ConfigAutoMapper.cs
@@ -1,6 +1,13 @@
 ﻿using AutoMapper;
 using BudgetAppBackend.Application.DTOs.AuthenticationDTOs;
+using BudgetAppBackend.Application.DTOs.BudgetDTOs;
+using BudgetAppBackend.Application.DTOs.TransactionDTOs;
+using BudgetAppBackend.Domain.BudgetAggregate;
+using BudgetAppBackend.Domain.BudgetAggregate.ValueObjects;
+using BudgetAppBackend.Domain.CategoryAggregate;
+using BudgetAppBackend.Domain.TransactionAggregate;
 using BudgetAppBackend.Domain.UserAggregate;
+using BudgetAppBackend.Domain.UserAggregate.ValueObjects;
 
 namespace BudgetAppBackend.Application.AutoMapper
 {
@@ -9,7 +16,32 @@ namespace BudgetAppBackend.Application.AutoMapper
         public ConfigAutoMapper()
         {            
             CreateMap<AddUserDto, User>().ReverseMap();
- 
+
+
+            CreateMap<CreateTransactionDto, Transaction>()
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => CategoryId.Create(src.CategoryId)))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => UserId.Create(src.UserId)));
+
+            CreateMap<CreateBudgetDto, Budget>()
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => CategoryId.Create(src.CategoryId)))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => UserId.Create(src.UserId)));
+
+            CreateMap<UpdateBudgetDto, Budget>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => BudgetId.Create(src.BudgetId)));
+
+
+            CreateMap<Budget, BudgetDto>()
+                .ForMember(dest => dest.BudgetId, opt => opt.MapFrom(src => src.Id.Id))
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId.Id))
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId.Id));
+
+            // Map Transaction → TransactionDto (Entity to DTO)
+            /*CreateMap<Transaction, TransactionDto>()
+                .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId.Value))
+                .ForMember(dest => dest.BudgetId, opt => opt.MapFrom(src => src.BudgetId != null ? src.BudgetId.Value : (Guid?)null));*/
+
+
+
         }
     }
 }
