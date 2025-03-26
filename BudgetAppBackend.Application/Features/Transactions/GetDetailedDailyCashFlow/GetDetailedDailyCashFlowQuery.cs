b920cb96abc31c5f1d273b16f82232580aba91f3
Abdalrahman H Bashir <127059452+AbdalrahmanBashir@users.noreply.github.com>
2025-03-25 21:56:57 -0500

    feat(cqrs): add GetDetailedDailyCashFlowQuery and handler for daily cash flow retrieval

---
+++ BudgetAppBackend.Application/Features/Transactions/GetDetailedDailyCashFlow/GetDetailedDailyCashFlowQuery.cs
@@ -0,0 +1,6 @@
+﻿using BudgetAppBackend.Application.DTOs.TransactionDTOs;
+using MediatR;
+
+namespace BudgetAppBackend.Application.Features.Transactions.GetDetailedDailyCashFlow;
+
+public sealed record GetDetailedDailyCashFlowQuery(Guid UserId, DateTime MonthStartDate) : IRequest<IEnumerable<DetailedDailyCashFlowDto>>;
---
+++ BudgetAppBackend.Application/Features/Transactions/GetDetailedDailyCashFlow/GetDetailedDailyCashFlowQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using BudgetAppBackend.Application.Contracts;
+using BudgetAppBackend.Application.DTOs.TransactionDTOs;
+using BudgetAppBackend.Domain.UserAggregate.ValueObjects;
+using MediatR;
+
+namespace BudgetAppBackend.Application.Features.Transactions.GetDetailedDailyCashFlow
+{
+    public class GetDetailedDailyCashFlowQueryHandler : IRequestHandler<GetDetailedDailyCashFlowQuery, IEnumerable<DetailedDailyCashFlowDto>>
+    {
+        private readonly ITransactionRepository _transactionReadRepository;
+        private readonly IPlaidTransactionRepository _plaidTransactionRepository;
+        public GetDetailedDailyCashFlowQueryHandler(
+            ITransactionRepository transactionReadRepository,
+            IPlaidTransactionRepository plaidTransactionRepository)
+        {
+            _transactionReadRepository = transactionReadRepository;
+            _plaidTransactionRepository = plaidTransactionRepository;
+        }
+        public async Task<IEnumerable<DetailedDailyCashFlowDto>> Handle(GetDetailedDailyCashFlowQuery request, CancellationToken cancellationToken)
+        {
+            var userId = UserId.Create(request.UserId);
+            var monthStartDate = request.MonthStartDate;
+            //var manualTransactions = await _transactionReadRepository.GetUserTransactionsQueryAsync(userId, cancellationToken);
+            var plaidTransactions = await _plaidTransactionRepository.GetDetailedDailyCashFlowAsync(userId, monthStartDate, cancellationToken);
+            // Combine and process transactions here
+            return plaidTransactions;
+        }
+    }
+    
+    
+}
