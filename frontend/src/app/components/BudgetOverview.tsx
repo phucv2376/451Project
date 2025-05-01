4f6e10352350a6a3433ab1be70c6ab1055d0c8c3
lisal00 <Lisam5022@gmail.com>
2025-05-01 15:23:04 -0500

    update budget overview page for smaller screens

--- frontend/src/app/components/BudgetOverview.tsx
+++ frontend/src/app/components/BudgetOverview.tsx
@@ -53,7 +53,7 @@ const BudgetOverview = () => {
     console.log("Budgets:", budgets);
 
     return (
-        <div className="w-full h-full lg:w-1/3 bg-white rounded-lg border border-gray-200 shadow-sm p-5">
+        <div className="w-full h-full md:w-5/12 lg:w-1/3 bg-white rounded-lg border border-gray-200 shadow-sm p-5">
             <h2 className="font-bold text-md mb-5">Budget Overview</h2>
             <div className="flex justify-center items-center h-full w-full">
                 <div className="grid grid-cols-2 gap-4 w-full h-full p-7">
